7eb7adf44c20957a9be8238c088f8dc72c7e2872
Janne Hellsten <jhellsten@nvidia.com>
2020-05-27 13:27:49 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+## MetFaces Dataset
+
+![Teaser image](./img/metfaces-teaser.png)
+
+MetFaces is an image dataset of human faces extracted from works of art.
+
+The dataset consists of 1336 high-quality PNG images at 1024&times;1024 resolution.  The images were downloaded via the [Metropolitan Museum of Art Collection API](https://metmuseum.github.io/), and automatically aligned and cropped using [dlib](http://dlib.net/).  Various automatic filters were used to prune the set.
+
+For business inquiries, please contact [researchinquiries@nvidia.com](mailto:researchinquiries@nvidia.com)
+
+For press and other inquiries, please contact Hector Marinez at [hmarinez@nvidia.com](mailto:hmarinez@nvidia.com)
+
+## Licenses
+
+The source images are made available under the [Creative Commons Zero (CC0)](https://creativecommons.org/publicdomain/zero/1.0/) license by the [Metropolitan Museum of Art](https://www.metmuseum.org/).  Please [read here](https://www.metmuseum.org/about-the-met/policies-and-documents/image-resources) for more information about their Open Access policy.
+
+The dataset itself (including JSON metadata, processed images, and documentation) is made available under [Creative Commons BY-NC 2.0](https://creativecommons.org/licenses/by-nc/2.0/) license by NVIDIA Corporation. You can **use, redistribute, and adapt it for non-commercial purposes**, as long as you (a) give appropriate credit by **citing our paper**, and (b) **indicate any changes** that you've made.
+
+
+## Overview
+
+All data is hosted on Google Drive:
+
+| Path | Size | Files | Format | Description
+| :--- | :--: | ----: | :----: | :----------
+| [metfaces-dataset](https://drive.google.com/open?id=1w-Os4uERBmXwCm7Oo_kW6X3Sd2YHpJMC) | 14.5 GB | 2621 | | Main folder
+| &boxvr;&nbsp;[metfaces.json](https://drive.google.com/open?id=1o11-JkkwBbZW61w03O7qGrhkydNALDSH) | 1.8 MB | 1 | JSON | Image metadata including original download URL.
+| &boxvr;&nbsp;[images](https://drive.google.com/open?id=1iChdwdW7mZFUyivKtDwL8ehCNhYKQz6D) | 1.6 GB | 1336 | PNG | Aligned and cropped images at 1024&times;1024
+| &boxur;&nbsp;[unprocessed](https://drive.google.com/open?id=1lut1g1oASGsipQQB67EFqVhjt4UgC5JW) | 13 GB | 1284 | PNG | Original images
+
+## Reproducing the dataset
+
+MetFaces 1024x1024 images can be reproduced with the `metfaces.py` script as follows:
+
+1. Download the contents of the metfaces-dataset Google Drive folder.  Retain the original folder structure (e.g., you should have `local/path/metfaces.json`, `local/path/unprocessed`.)
+2. Run `metfaces.py --json data/metfaces.json --source-images data --output-dir out`
+
+## Metadata
+
+The `metfaces.json` file contains the following information for each image:
+
+```
+[
+  {
+    "obj_id": "11713",                                  # Metmuseum object ID
+    "meta_url": "https://collectionapi.metmuseum.org/public/collection/v1/objects/11713",
+    "source_url": "https://images.metmuseum.org/CRDImages/ad/original/ap26.129.1.jpg",
+    "source_path": "unprocessed/image-11713.png",       # Original raw image file under local dataset copy
+    "source_md5": "c1e4c5a42de6a4d6909d3820c16f9eb5",   # MD5 checksum of the raw image file
+    "image_path": "images/11713-00.png",                # Processed 1024x1024 image
+    "image_md5": "605a90ab744bdbc9737da5620f2777ab",    # MD5 checksum of the processed image
+    "title": "Portrait of a Gentleman",                 # Metmuseum object's title
+    "artist_display_name": "Charles Willson Peale",     # Metmuseum object's artist's display name
+    "face_spec": {                                      # Info about the raw image:
+      "rect": [404, 238, 775, 610],                     # - Axis-aligned rectangle of the face region
+      "landmarks": [...],                               # - 68 face landmarks reported by dlib
+      "shrink": 2
+    },
+    "face_idx": 0
+  },
+  ...
+]
+```
+
+For full Metmuseum metadata, you can access the `meta_url` contents by e.g., `curl https://collectionapi.metmuseum.org/public/collection/v1/objects/11713`.
---
+++ metfaces.py
@@ -0,0 +1,129 @@
+# Copyright 2020 NVIDIA Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import json
+import numpy as np
+import os
+import PIL.Image
+import scipy.ndimage
+from tqdm import tqdm
+
+_examples = '''examples:
+
+  # Run x, y, z
+  python %(prog)s --output=tmp
+'''
+
+def extract_face(face, source_images, output_dir, target_size=1024, supersampling=4, enable_padding=True):
+    def rot90(v) -> np.ndarray:
+        return np.array([-v[1], v[0]])
+
+    # Sanitize facial landmarks.
+    face_spec = face['face_spec']
+    landmarks = (np.float32(face_spec['landmarks']) + 0.5) * face_spec['shrink']
+    assert landmarks.shape == (68, 2)
+    lm_eye_left      = landmarks[36 : 42]  # left-clockwise
+    lm_eye_right     = landmarks[42 : 48]  # left-clockwise
+    lm_mouth_outer   = landmarks[48 : 60]  # left-clockwise
+
+    # Calculate auxiliary vectors.
+    eye_left     = np.mean(lm_eye_left, axis=0)
+    eye_right    = np.mean(lm_eye_right, axis=0)
+    eye_avg      = (eye_left + eye_right) * 0.5
+    eye_to_eye   = eye_right - eye_left
+    mouth_left   = lm_mouth_outer[0]
+    mouth_right  = lm_mouth_outer[6]
+    mouth_avg    = (mouth_left + mouth_right) * 0.5
+    eye_to_mouth = mouth_avg - eye_avg
+
+    # Choose oriented crop rectangle.
+    x = eye_to_eye - rot90(eye_to_mouth)
+    x /= np.hypot(*x)
+    x *= max(np.hypot(*eye_to_eye) * 2.0, np.hypot(*eye_to_mouth) * 1.8)
+    y = rot90(x)
+    c = eye_avg + eye_to_mouth * 0.1
+
+    # Calculate auxiliary data.
+    qsize = np.hypot(*x) * 2
+    quad = np.stack([c - x - y, c - x + y, c + x + y, c + x - y])
+    lo = np.min(quad, axis=0)
+    hi = np.max(quad, axis=0)
+    lm_rel = np.dot(landmarks - c, np.transpose([x, y])) / qsize**2 * 2 + 0.5
+    rp = np.dot(np.random.RandomState(123).uniform(-1, 1, size=(1024, 2)), [x, y]) + c
+
+    # Load.
+    img = PIL.Image.open(os.path.join(source_images, face['source_path'])).convert('RGB')
+
+    # Shrink.
+    shrink = int(np.floor(qsize / target_size * 0.5))
+    if shrink > 1:
+        rsize = (int(np.rint(float(img.size[0]) / shrink)), int(np.rint(float(img.size[1]) / shrink)))
+        img = img.resize(rsize, PIL.Image.ANTIALIAS)
+        quad /= shrink
+        qsize /= shrink
+
+    # Crop.
+    border = max(int(np.rint(qsize * 0.1)), 3)
+    crop = (int(np.floor(min(quad[:,0]))), int(np.floor(min(quad[:,1]))), int(np.ceil(max(quad[:,0]))), int(np.ceil(max(quad[:,1]))))
+    crop = (max(crop[0] - border, 0), max(crop[1] - border, 0), min(crop[2] + border, img.size[0]), min(crop[3] + border, img.size[1]))
+    if crop[2] - crop[0] < img.size[0] or crop[3] - crop[1] < img.size[1]:
+        img = img.crop(crop)
+        quad -= crop[0:2]
+
+    # Pad.
+    pad = (int(np.floor(min(quad[:,0]))), int(np.floor(min(quad[:,1]))), int(np.ceil(max(quad[:,0]))), int(np.ceil(max(quad[:,1]))))
+    pad = (max(-pad[0] + border, 0), max(-pad[1] + border, 0), max(pad[2] - img.size[0] + border, 0), max(pad[3] - img.size[1] + border, 0))
+    if enable_padding and max(pad) > border - 4:
+        pad = np.maximum(pad, int(np.rint(qsize * 0.3)))
+        img = np.pad(np.float32(img), ((pad[1], pad[3]), (pad[0], pad[2]), (0, 0)), 'reflect')
+        h, w, _ = img.shape
+        y, x, _ = np.ogrid[:h, :w, :1]
+        mask = np.maximum(1.0 - np.minimum(np.float32(x) / pad[0], np.float32(w-1-x) / pad[2]), 1.0 - np.minimum(np.float32(y) / pad[1], np.float32(h-1-y) / pad[3]))
+        blur = qsize * 0.02
+        img += (scipy.ndimage.gaussian_filter(img, [blur, blur, 0]) - img) * np.clip(mask * 3.0 + 1.0, 0.0, 1.0)
+        img += (np.median(img, axis=(0,1)) - img) * np.clip(mask, 0.0, 1.0)
+        img = PIL.Image.fromarray(np.uint8(np.clip(np.rint(img), 0, 255)), 'RGB')
+        quad += pad[:2]
+
+    # Transform.
+    super_size = target_size * supersampling
+    img = img.transform((super_size, super_size), PIL.Image.QUAD, (quad + 0.5).flatten(), PIL.Image.BILINEAR)
+    if target_size < super_size:
+        img = img.resize((target_size, target_size), PIL.Image.ANTIALIAS)
+
+    # Save face image.
+    img.save(os.path.join(output_dir, f"{face['obj_id']}-{face['face_idx']:02d}.png"))
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='MetFaces dataset processing tool',
+        epilog=_examples,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument('--json', help='MetFaces metadata json file path', required=True)
+    parser.add_argument('--source-images', help='Location of MetFaces raw image data', required=True)
+    parser.add_argument('--output-dir', help='Where to save output files')
+    args = parser.parse_args()
+
+    os.makedirs(args.output_dir, exist_ok=True)
+
+    with open(args.json) as fin:
+        faces = json.load(fin)
+        for f in tqdm(faces):
+            extract_face(f, source_images=args.source_images, output_dir=args.output_dir)
+
+if __name__ == "__main__":
+    main()
